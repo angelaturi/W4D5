0941cae0f534d4a7c27b199425eda1012e9a4569
aturi <angelaturi99@gmail.com>
2021-03-19 13:59:14 -0400

    Add phase 2 largest_subsum

--- time_difference.rb
+++ time_difference.rb
@@ -32,17 +32,68 @@ end
 list = [ 0, 3, 5, 4, -5, 10, 1, 90 ]
 p my_min2(list)  # =>  -5
 
-def largest_subsum(list)
+# def largest_subsum(list)
 
-    subs = []
-    (0...list.length).each do |idx1|
-        (idx1...list.length).each do |idx2|
-            subs << list[idx1..idx2]
+#     subs = [] # O(1)
+#     (0...list.length).each do |idx1| # O(n)
+#         (idx1...list.length).each do |idx2| # O(n)
+#             subs << list[idx1..idx2] # O(1)
+#         end
+#     end
+#     subs.map {|sub| sub.sum }.max # O(n)
+
+# end
+
+# O(1) + O(n) * O(n)* O(1) + O(n)
+
+# O(n^2)
+
+list = [5, 3, -7]
+# p largest_subsum(list) # => 8
+
+# def largest_subsum(list)
+#     max = list[0] 
+#     current_sum = list[0]
+
+#     list.each_with_index do |ele, i|
+#         next_num = list[i + 1]
+#         current_sum = ele + next_num
+#         if current_sum > max
+#             max = current_sum
+#         end
+
+#         if current_sum < 0
+#             current_sum = 0
+#         end
+#     end
+
+#     max
+
+# end
+
+def largest_subsum(list)
+    max = list[0] 
+    current_sum = list[0]
+
+    (1...list.length).each do |i|
+        current_sum += list[i]
+        if current_sum > max
+            max = current_sum
+        end
+        if current_sum < 0
+            current_sum = 0
         end
     end
-    subs.map {|sub| sub.sum }.max
+
+    max
 
 end
 
 list = [5, 3, -7]
 p largest_subsum(list) # => 8
+
+list = [2, 3, -6, 7, -6, 7]
+p largest_subsum(list) # => 8 (from [7, -6, 7])
+
+list = [-5, -1, -3]
+p largest_subsum(list) # => -1 (from [-1])
