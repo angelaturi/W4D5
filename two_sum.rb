b4ffecb91614aad990b3acbc7497a81e918610c9
Daniel Cho <thedanielcho@gmail.com>
2021-03-19 17:35:14 -0400

    Finish four_sum?

--- two_sum.rb
+++ two_sum.rb
@@ -14,7 +14,7 @@ arr = [0, 1, 5, 7]
 # p bad_two_sum?(arr, 10) # => should be false
 
 def okay_two_sum?(arr, target)
-    sorted = arr.sort # O(n^2)
+    sorted = arr.sort # O(nlog(n))
     i = 0 # O(1)
     k = arr.length - 1 # O(1)
     while i < k # O(n)
@@ -29,7 +29,18 @@ def okay_two_sum?(arr, target)
     return false # O(1)
 end
 
-# O(n^2)
+# def sum?(arr, target)
+#     sorted = arr.sort
+#     sorted.each_with_index do |ele,idx|
+#         answer = sorted.bsearch(target - ele)
+#             return true if answer != idx && answer != nil
+#         end
+    
+#     end
+#     return false
+# end
+
+# O(nlog(n))
 
 # arr = [0, 1, 5, 7]
 
@@ -38,17 +49,40 @@ end
 
 
 def two_sum?(arr, target)
-    hash = {}
+    hash = {} #0(1)
 
-    arr.each do |ele|
-        return true if hash.has_key?(target - ele)
-        hash[ele] = true 
+    arr.each do |ele| #0(n)
+        return true if hash[target - ele] #0(1)
+        hash[ele] = true #0(1)
     end
 
+    false#0(1)
+end
+
+#0(n)
+
+# # arr = [0, 1, 5, 7]
+# p two_sum?(arr, 6) # => should be true
+# p two_sum?(arr, 10) # => should be false
+
+def four_sum?(arr, target)
+
+    hash = {} # checks elements
+    hash2 = {} # contains sums of two elements from arr
+    hash3 = {} # contains sums of three elements from arr
+    arr.each do |ele|
+        return true if hash3[target-ele]
+        hash2.each_key do |key|
+            hash3[key + ele] = true
+        end
+        hash.each_key do |k|
+            hash2[k+ele] = true
+        end
+        hash[ele] = true
+    end
     false
 end
 
 
-arr = [0, 1, 5, 7]
-p two_sum?(arr, 6) # => should be true
-p two_sum?(arr, 10) # => should be false
+arr = [0, 1, 5, 7, 10, 12, 15]
+p four_sum?(arr,64) 
